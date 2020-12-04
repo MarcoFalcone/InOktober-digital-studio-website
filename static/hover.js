eadd8dd580af8af96377afb3b4d7ac883479c584
Markrius <marco.falcone@hotmail.it>
2020-12-04 20:09:50 +0100

    work in progress

--- static/hover.js
+++ static/hover.js
@@ -4,7 +4,7 @@ var myAnimation = new hoverEffect({
     intensity: 0.3,
     image1: "https://ik.imagekit.io/7ol7k87qcca/InOktober/Collabs/index_f1wYvf-Mdk7aH.jpg",
     image2: "https://ik.imagekit.io/7ol7k87qcca/InOktober/Collabs/index5_Np1w5eAkLAytx.jpg",
-    displacementImage: 'https://ik.imagekit.io/7ol7k87qcca/InOktober/hover1_qPv3tWGZ6VXBY.jpg'
+    displacementImage: 'https://ik.imagekit.io/7ol7k87qcca/InOktober/hover1_TYxLhGMwWv5r.jpg?tr=h-600,w-600'
 });
 
 var myAnimation = new hoverEffect({
@@ -12,7 +12,7 @@ var myAnimation = new hoverEffect({
     intensity: 0.3,
     image1: "https://ik.imagekit.io/7ol7k87qcca/InOktober/Interiors/TYLKO_RENDER_X8Q38TMpPO8V.png",
     image2: "https://ik.imagekit.io/7ol7k87qcca/InOktober/Interiors/corner_2_dfyDWAwPiLMB2.png",
-    displacementImage: 'https://ik.imagekit.io/7ol7k87qcca/InOktober/hover2_exgE1thHJPnA1.jpg'
+    displacementImage: 'https://ik.imagekit.io/7ol7k87qcca/InOktober/hover2_exgE1thHJPnA1.jpg?tr=h-600,w-600'
 });
 
 var myAnimation = new hoverEffect({
@@ -20,5 +20,5 @@ var myAnimation = new hoverEffect({
     intensity: 0.3,
     image1: "https://ik.imagekit.io/7ol7k87qcca/InOktober/Artwork/heart_WCXUgTpcHFSM4.png",
     image2: "https://ik.imagekit.io/7ol7k87qcca/InOktober/Artwork/face_kLAEZuYAwcY3_.png",
-    displacementImage: 'https://ik.imagekit.io/7ol7k87qcca/InOktober/hover3_nlP-5F9FT9yDt.jpg'
+    displacementImage: 'https://ik.imagekit.io/7ol7k87qcca/InOktober/hover3_nlP-5F9FT9yDt.jpg?tr=h-600,w-600'
 });
