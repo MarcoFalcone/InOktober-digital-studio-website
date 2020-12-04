8f50d53f81cd6419f24c02fab2e8a304b8ce1eef
Markrius <marco.falcone@hotmail.it>
2020-12-04 04:06:51 +0100

    work in progress

--- static/app.js
+++ static/app.js
@@ -50,13 +50,30 @@ $(".contactform").hover(function(){
   $(".contactform").removeClass("pointerevent")
 })
 
+//show title and transform//
+$(".work1").hover(function(){
+  $(".collabs").addClass("fadein")
+  $(".work1").addClass("transform")
+},function(){
+  $(".collabs").removeClass("fadein");
+  $(".work1").removeClass("transform");
+})
+$(".work2").hover(function(){
+  $(".interiors").addClass("fadein")
+  $(".work2").addClass("transform")
+},function(){
+  $(".interiors").removeClass("fadein");
+  $(".work2").removeClass("transform");
+})
+$(".work3").hover(function(){
+  $(".artwork").addClass("fadein")
+  $(".work3").addClass("transform")
+},function(){
+  $(".artwork").removeClass("fadein");
+  $(".work3").removeClass("transform");
+})
+
 //reload when changing orientation on mobile//
 $(window).bind('orientationchange', function (event) {
     location.reload(true);
 });
-
-//prevent the softkeyboard to change layout on android//
-var meta = document.createElement('meta');
-meta.name = 'viewport';
-meta.content = 'width=device-width,height='+window.innerHeight+', initial-scale=1.0';
-document.getElementsByTagName('head')[0].appendChild(meta);
