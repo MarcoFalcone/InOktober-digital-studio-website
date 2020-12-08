f0925de517b429dd4327bd0516356726019936e1
Markrius <marco.falcone@hotmail.it>
2020-12-08 02:56:04 +0100

    work in progress

--- static/app.js
+++ static/app.js
@@ -2,8 +2,9 @@
 jQuery('.work1, .work2, .work3, .backhome, .backhome2').click(function(e) {
   e.preventDefault();
   newLocation = this.href;
-  jQuery('body').fadeOut('slow', newpage);
+  jQuery('body').fadeOut(newpage);
 });
+
 function newpage() {
   window.location = newLocation;
 }
@@ -16,64 +17,63 @@ $(window).bind("pageshow", function(event) {
 });
 
 //hide anchor-link on the url on mobile and smoothscroll//
-$('.down1').click(function (e) {
-    e.preventDefault();
+$('.down1').click(function(e) {
+  e.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $("#txt").offset().top - 0
-    }, 600);
+  $('html, body').animate({
+    scrollTop: $("#txt").offset().top - 0
+  }, 600);
 });
-$('.down2').click(function (e) {
-    e.preventDefault();
+$('.down2').click(function(e) {
+  e.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $("#form").offset().top - 0
-    }, 600);
+  $('html, body').animate({
+    scrollTop: $("#form").offset().top - 0
+  }, 600);
 });
 
 //hideforaboutandcontact//
-$(".about").hover(function(){
+$(".about").hover(function() {
   $(".workscont").addClass("hideworks")
   $(".abouttxt").addClass("showabout")
-},function(){
+}, function() {
   $(".workscont").removeClass("hideworks")
   $(".abouttxt").removeClass("showabout");
 })
-$(".contact").hover(function(){
+$(".contact").hover(function() {
   $(".workscont").addClass("hideworks")
   $(".contactform").addClass("pointerevent") //if pointer quickly hovers again on the form it interrupts the fade out because of the .8s transition, this fixes that making a clean fade out//
-},function(){
+}, function() {
   $(".workscont").removeClass("hideworks")
 })
-$(".contactform").hover(function(){
-},function(){
+$(".contactform").hover(function() {}, function() {
   $(".contactform").removeClass("pointerevent")
 })
 
 //show title and transform//
-$(".work1").hover(function(){
+$(".work1").hover(function() {
   $(".collabs").addClass("fadein")
-  $(".work1").addClass("transform")
-},function(){
+  $(".first").addClass("transform")
+}, function() {
   $(".collabs").removeClass("fadein");
-  $(".work1").removeClass("transform");
+  $(".first").removeClass("transform");
 })
-$(".work2").hover(function(){
+$(".work2").hover(function() {
   $(".interiors").addClass("fadein")
-  $(".work2").addClass("transform")
-},function(){
+  $(".second").addClass("transform")
+}, function() {
   $(".interiors").removeClass("fadein");
-  $(".work2").removeClass("transform");
+  $(".second").removeClass("transform");
 })
-$(".work3").hover(function(){
+$(".work3").hover(function() {
   $(".artwork").addClass("fadein")
-  $(".work3").addClass("transform")
-},function(){
+  $(".third").addClass("transform")
+}, function() {
   $(".artwork").removeClass("fadein");
-  $(".work3").removeClass("transform");
+  $(".third").removeClass("transform");
 })
 
 //reload when changing orientation on mobile//
-$(window).bind('orientationchange', function (event) {
-    location.reload(true);
+$(window).bind('orientationchange', function(event) {
+  location.reload(true);
 });
