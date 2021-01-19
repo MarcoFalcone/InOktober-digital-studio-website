4d286ec8bafe95b7e7a2059b825c9d993048ac27
Markrius <marco.falcone@hotmail.it>
2021-01-19 21:19:27 +0100

    cleaned js

--- static/app.js
+++ static/app.js
@@ -25,61 +25,67 @@ $(window).bind("pageshow", function(event) {
 });
 
 //hide anchor-link on the url on mobile and smoothscroll//
-$('.down1').click(function(e) {
-  e.preventDefault();
+$(document).ready(function(){
+  $('.down1').click(function(e) {
+    e.preventDefault();
 
-  $('html, body').animate({
-    scrollTop: $("#txt").offset().top - 0
-  }, 600);
-});
-$('.down2').click(function(e) {
-  e.preventDefault();
+    $('html, body').animate({
+      scrollTop: $("#txt").offset().top - 0
+    }, 600);
+  });
+  $('.down2').click(function(e) {
+    e.preventDefault();
 
-  $('html, body').animate({
-    scrollTop: $("#mail").offset().top - 0
-  }, 600);
+    $('html, body').animate({
+      scrollTop: $("#mail").offset().top - 0
+    }, 600);
+  });
 });
 
 //hide for about and contact//
-$(".about").hover(function() {
-  $(".workscont").addClass("hideworks")
-  $(".abouttxt").addClass("showabout")
-}, function() {
-  $(".workscont").removeClass("hideworks")
-  $(".abouttxt").removeClass("showabout");
-})
-$(".contact").hover(function() {
-  $(".workscont").addClass("hideworks")
-  $(".contactcont").addClass("pointerevent") //if cursor quickly hovers again on the form it interrupts the fade out because of the .8s transition, this fixes that making a clean fade out//
-}, function() {
-  $(".workscont").removeClass("hideworks")
-})
-$(".contactcont").hover(function() {}, function() {
-  $(".contactcont").removeClass("pointerevent")
-})
+$(document).ready(function(){
+  $(".about").hover(function() {
+    $(".workscont").addClass("hideworks")
+    $(".abouttxt").addClass("showabout")
+  }, function() {
+    $(".workscont").removeClass("hideworks")
+    $(".abouttxt").removeClass("showabout");
+  })
+  $(".contact").hover(function() {
+    $(".workscont").addClass("hideworks")
+    $(".contactcont").addClass("pointerevent") //if cursor quickly hovers again on the form it interrupts the fade out because of the .8s transition, this fixes that making a clean fade out//
+  }, function() {
+    $(".workscont").removeClass("hideworks")
+  })
+  $(".contactcont").hover(function() {}, function() {
+    $(".contactcont").removeClass("pointerevent")
+  })
+});
 
 //show title and transform//
-$(".work1").hover(function() {
-  $(".collabs").addClass("fadein")
-  $(".first").addClass("transform")
-}, function() {
-  $(".collabs").removeClass("fadein");
-  $(".first").removeClass("transform");
-})
-$(".work2").hover(function() {
-  $(".interiors").addClass("fadein")
-  $(".second").addClass("transform")
-}, function() {
-  $(".interiors").removeClass("fadein");
-  $(".second").removeClass("transform");
-})
-$(".work3").hover(function() {
-  $(".artwork").addClass("fadein")
-  $(".third").addClass("transform")
-}, function() {
-  $(".artwork").removeClass("fadein");
-  $(".third").removeClass("transform");
-})
+$(document).ready(function(){
+  $(".work1").hover(function() {
+    $(".collabs").addClass("fadein")
+    $(".first").addClass("transform")
+  }, function() {
+    $(".collabs").removeClass("fadein");
+    $(".first").removeClass("transform");
+  })
+  $(".work2").hover(function() {
+    $(".interiors").addClass("fadein")
+    $(".second").addClass("transform")
+  }, function() {
+    $(".interiors").removeClass("fadein");
+    $(".second").removeClass("transform");
+  })
+  $(".work3").hover(function() {
+    $(".artwork").addClass("fadein")
+    $(".third").addClass("transform")
+  }, function() {
+    $(".artwork").removeClass("fadein");
+    $(".third").removeClass("transform");
+  })
+});
 
 //reload when changing orientation on mobile//
 $(window).bind('orientationchange', function(event) {
